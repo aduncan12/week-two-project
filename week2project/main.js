6b3e30c75b51dd827f63d6f78b5720c97b6618c0
Alan <a386226@gmail.com>
2018-08-11 22:53:42 -0700

    started JS file, added images

---
+++ week2project/main.js
@@ -0,0 +1,51 @@
+var breakfastItems = [
+    {   img: './assets/Breakfast/bacon.png',
+        name: 'Bacon'},
+    {   img: './assets/Breakfast/eggs.jpeg',
+        name: 'Eggs'},
+    {   img: './assets/Breakfast/pancakes.jpeg',
+        name: 'Pancakes'},
+    {   img: './assets/Breakfast/fruit.jpeg',
+        name: 'Fruit'},
+    {   img: './assets/Breakfast/biscuits.jpeg',
+        name: 'Biscuits'},
+    {   img: './assets/Breakfast/waffles.png',
+        name: 'Waffles'},        
+    {   img: './assets/Breakfast/toast.jpeg',
+        name: 'Toast'},
+    {   img: './assets/Breakfast/homefries.jpeg',
+        name: 'Home Fries'},                        
+    ];
+
+var lunchItems = [
+    {   img: './assets/Lunch/burger.jpeg',
+        name: 'Cheeseburger'},
+    {   img: './assets/Lunch/clubsandwich.jpeg',
+        name: 'Club Sandwich'},
+    {   img: './assets/Lunch/grilledcheese.jpeg',
+        name: 'Grilled Cheese Sandwich'},
+    {   img: './assets/Lunch/salad.jpeg',
+        name: 'Salad'},
+    {   img: './assets/Lunch/fries.jpeg',
+        name: 'French Fries'},
+    {   img: './assets/Lunch/pastasalad.jpeg',
+        name: 'Pasta Salad'}        
+];
+
+var dinnerItems = [
+    {   img: './assets/Dinner/chicken.jpeg',
+        name: 'Grilled Chicken'},
+    {   img: './assets/Dinner/chefsalad.jpeg',
+        name: 'Chef Salad'},
+    {   img: './assets/Dinner/potato.jpeg',
+        name: 'Baked Potato'},
+    {   img: './assets/Dinner/steak.png',
+        name: 'Steak'},
+    {   img: './assets/Dinner/pork.jpeg',
+        name: 'Pork Chops'},
+    {   img: './assets/Dinner/soup.jpeg',
+        name: 'Tomato Soup'},
+    {   img: './assets/Dinner/salmon.jpeg',
+        name: 'Salmon'},
+];
+
